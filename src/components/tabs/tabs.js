eb55dcfe13f7fcb8a1c6202e12eb9377e01010a3
Maxim Toropchin <vimevi57@gmail.com>
2024-01-08 01:57:34 +0300

    configs installed

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -4,41 +4,41 @@ import { useDispatch, useSelector } from 'react-redux';
 import { cheapest, shortest, resetFilter } from '../../redux/actions/sort';
 
 export default function Tabs() {
-  const dispatch = useDispatch();
-  const currentFilter = useSelector((state) => state.filter.filter);
+	const dispatch = useDispatch();
+	const currentFilter = useSelector((state) => state.filter.filter);
 
-  const handleButtonClick = (filter) => {
-    if (currentFilter === filter) {
-      dispatch(resetFilter());
-    } else {
-      dispatch(filter === 'cheapest' ? cheapest() : shortest());
-    }
-  };
+	const handleButtonClick = (filter) => {
+		if (currentFilter === filter) {
+			dispatch(resetFilter());
+		} else {
+			dispatch(filter === 'cheapest' ? cheapest() : shortest());
+		}
+	};
 
-  return (
-    <section className={classes.tabs}>
-      <nav>
-        <button
-          className={
-            currentFilter === 'cheapest'
-              ? `${classes.button} ${classes.active}`
-              : classes.button
-          }
-          onClick={() => handleButtonClick('cheapest')}
-        >
-          Самый Дешевый
-        </button>
-        <button
-          className={
-            currentFilter === 'shortest'
-              ? `${classes.button} ${classes.active}`
-              : classes.button
-          }
-          onClick={() => handleButtonClick('shortest')}
-        >
-          Самый быстрый
-        </button>
-      </nav>
-    </section>
-  );
+	return (
+		<section className={classes.tabs}>
+			<nav>
+				<button
+					className={
+						currentFilter === 'cheapest'
+							? `${classes.button} ${classes.active}`
+							: classes.button
+					}
+					onClick={() => handleButtonClick('cheapest')}
+				>
+					Самый Дешевый
+				</button>
+				<button
+					className={
+						currentFilter === 'shortest'
+							? `${classes.button} ${classes.active}`
+							: classes.button
+					}
+					onClick={() => handleButtonClick('shortest')}
+				>
+					Самый быстрый
+				</button>
+			</nav>
+		</section>
+	);
 }
